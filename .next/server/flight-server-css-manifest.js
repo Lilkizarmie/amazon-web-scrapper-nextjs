3f00d540bb8f6d593cb823561ae9c4f1d6158158
Jeffery Kizarmie Adolor <jefferyadolor18@gmail.com>
2023-03-24 08:56:46 +0100

    restructured project

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,8 @@
+self.__RSC_CSS_MANIFEST={
+  "__entry_css_mods__": {
+    "/Users/lilkizarmie/Documents/GitHub/amazon-web-scrapper-nextjs/app/page": []
+  },
+  "/Users/lilkizarmie/Documents/GitHub/amazon-web-scrapper-nextjs/app/layout.tsx": [
+    "/Users/lilkizarmie/Documents/GitHub/amazon-web-scrapper-nextjs/styles/globals.css"
+  ]
+}
