ecede6a23dea631df4eb886470c681640a9d2e03
Jeffery Kizarmie Adolor <jefferyadolor18@gmail.com>
2023-04-07 01:01:48 +0100

    ran test tunneling

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -1,9 +1,13 @@
 import * as functions from "firebase-functions";
 
-// // Start writing functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// Start writing functions
+// https://firebase.google.com/docs/functions/typescript
+
+export const onScraperComplete = functions.https.onRequest((request, response) => {
+  console.log("SCRAPPER COMPLETE >>> : ", request.body);
+  
+  functions.logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
+});
+
+// https://b389-102-89-42-186.eu.ngrok.io/flash-buy-97128/us-central1/onScraperComplete
